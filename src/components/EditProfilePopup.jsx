9c6c5e5c6e8d9aa568173036ccc88c8d21737b05
Bl Bltich <vissar2016@yandex.ru>
2023-08-29 02:11:20 +0300

    fix: Edit componend updated

--- src/components/EditProfilePopup.jsx
+++ src/components/EditProfilePopup.jsx
@@ -1,29 +1,11 @@
 import React, { useEffect, useState, useContext, useRef } from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import PopupWithForm from "./PopupWithForm";
-import FormValidator from "../utils/FormValidator.js";
-import { validationConfig } from "../utils/constants.js";
-
 
 export default function EditProfilePopup({ isOpen, onClose, onUpdateUser, isLoading }) {
     const [profileName, setName] = useState('');
     const [profileDescription, setDescription] = useState('');
     const currentUser = useContext(CurrentUserContext);
-    const formRef = useRef(null);
-    const nameValue = useRef();
-    const descriptionValue = useRef();
-    const formValidation = useRef(null);
-
-    useEffect(() => {
-        if (isOpen && formRef.current) {
-            if (!formValidation.current) {
-                formValidation.current = new FormValidator(validationConfig, formRef.current);
-                formValidation.current.enableValidation();
-            } else {
-                formValidation.current.resetValidationState();
-            }
-        }
-    }, [isOpen])
 
     useEffect(() => {
         setName(currentUser.name);
@@ -40,7 +22,6 @@ export default function EditProfilePopup({ isOpen, onClose, onUpdateUser, isLoad
 
     return (
         <PopupWithForm
-            ref={formRef}
             name='edit'
             title='Редактировать профиль'
             isOpen={isOpen}
@@ -51,7 +32,6 @@ export default function EditProfilePopup({ isOpen, onClose, onUpdateUser, isLoad
             isLoading={isLoading}>
             <div htmlFor="popup__input_type_name" className="popup__field">
                 <input type="text"
-                    ref={nameValue}
                     name="name"
                     className="popup__input popup__input_type_name"
                     placeholder="Введите имя профиля" id="name-input"
@@ -63,7 +43,6 @@ export default function EditProfilePopup({ isOpen, onClose, onUpdateUser, isLoad
             </div>
             <div htmlFor="popup__input_type_description" className="popup__field">
                 <input type="text"
-                    ref={descriptionValue}
                     name="about"
                     className="popup__input popup__input_type_description"
                     placeholder="Введите описание профиля"
